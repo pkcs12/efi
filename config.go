bbe7a21d556d60be1fa0787ce3d4e1ee98218861
Valerii Lider <valerii.lider@gmail.com>
2021-02-16 16:18:30 +0100

    config.go added

---
+++ config.go
@@ -0,0 +1,32 @@
+package efi
+
+type EnvironmentType string
+
+// EnvironmentType constants
+const (
+	TEST EnvironmentType = "TEST"
+	PROD EnvironmentType = "PROD"
+)
+
+// Config represents configuration that is used by efi modules
+type Config struct {
+	// Company details
+	TIN     string `json:"TIN"`
+	Name    string `json:"Name"`
+	VAT     string `json:"VAT"`
+	Address string `json:"Address"`
+	Town    string `json:"Town"`
+	Country string `json:"Country"`
+	// Environment, eg staging or production
+	Environment EnvironmentType `json:"Environment"`
+	// EFI-related constants
+	BusinUnitCode string `json:"BusinUnitCode"`
+	TCRCode       string `json:"TCRCode"`
+	SoftCode      string `json:"SoftCode"`
+	OperatorCode  string `json:"OperatorCode"`
+	// Typless constnts
+	Typless struct {
+		APIKey   string `json:"APIKey"`
+		Template string `json:"Template"`
+	} `json:"Typless"`
+}
